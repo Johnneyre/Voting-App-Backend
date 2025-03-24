ccbdaadfe03d01e3ccb57bc8da6c753d10bd8217
Johnneyre <johnneyrez@gmail.com>
2025-03-24 14:04:46 -0400

    refactor: use OptionVote like Embeddable, create getAll and getById methods in controller

--- src/main/java/com/voting/votingApp/controllers/PollController.java
+++ src/main/java/com/voting/votingApp/controllers/PollController.java
@@ -3,10 +3,10 @@ package com.voting.votingApp.controllers;
 import com.voting.votingApp.model.Poll;
 import com.voting.votingApp.services.PollServices;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/polls")
@@ -17,8 +17,21 @@ public class PollController {
         this.pollServices = pollServices;
     }
 
+    @GetMapping
+    public List<Poll> getAllPolls() {
+        return pollServices.getAllPolls();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Poll> getPoll(@PathVariable Long id) {
+        return pollServices.getPollById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
     @PostMapping
     public Poll createPoll(@Valid @RequestBody Poll poll) {
         return pollServices.createPoll(poll);
     }
 }
+
--- src/main/java/com/voting/votingApp/services/PollServices.java
+++ src/main/java/com/voting/votingApp/services/PollServices.java
@@ -2,7 +2,11 @@ package com.voting.votingApp.services;
 
 import com.voting.votingApp.model.Poll;
 import com.voting.votingApp.repositories.PollRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PollServices {
@@ -14,5 +18,13 @@ public class PollServices {
 
     public Poll createPoll(Poll poll) {
         return pollRepository.save(poll);
+    }
+
+    public List<Poll> getAllPolls() {
+        return pollRepository.findAll();
+    }
+
+    public Optional<Poll> getPollById(long id) {
+        return pollRepository.findById(id);
     }
 }
